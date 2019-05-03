db1aeb7deeb8e39019c048fbf6e59944a2aadf35
ArtoLabs <artopium@gmail.com>
2019-05-03 05:52:53 -0600

    Change directory name
    also add setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    python_requires='>=3.0',
+    name='MySimpleDB',
+    version='1.6',
+    packages=['mysimpledb'],
+    license='MIT',
+    keywords='pymysql, mysql, database',
+    url='http://github.com/artolabs/mysimpledb',
+    author='ArtoLabs',
+    author_email='artopium@gmail.com',
+    install_requires=[
+        'screenlogger',
+    ],
+    py_modules=['mysimpledb'],
+    include_package_data=True,
+    zip_safe=False
+)
